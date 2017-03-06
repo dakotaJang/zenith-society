2c95a6b236b38c1213a08dada564d2ea4b30db37
Junhyuk Jang <dakotajang@junhyuks-air.wifi.bcit.ca>
2017-03-06 12:32:45 -0800

    fix untracked files

---
+++ docs/dbSeed.js
@@ -0,0 +1,66 @@
+var connection = new Mongo( "ds113580.mlab.com:13580" );
+var db = connection.getDB( "zenithdb" );
+var authentication = db.auth( "user", "123456" );
+if(authentication){
+    db.activities.remove({});
+    print( "activities collection has been reset." );
+
+    var creationDate = new Date();
+    // var creationDate = new Date("2017-03-01T08:30:00.00Z");
+    db.activities.insert(
+    [{
+        "Description":"Senior’s  Golf Tournament",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Leadership General Assembly Meeting",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Youth Bowling Tournament",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Young ladies cooking lessons",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Youth craft lessons",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Youth choir practice",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Lunch",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Pancake Breakfast",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Swimming Lessons for the youth",
+        "CreationDate": creationDate
+    },
+
+    {
+        "Description":"Swimming Exercise for parents",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Bingo Tournament",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"BBQ Lunch",
+        "CreationDate": creationDate
+    },
+    {
+        "Description":"Garage Sale",
+        "CreationDate": creationDate
+    }
+    ]);
+    print( "activities collection has been repopulated." );
+}
